6a7dee9bffc5254b26f2f3bd837475f1b87805a9
Favoursimeon <favoursimeon12w@gmail.com>
2024-03-06 17:17:14 -0800

    commm

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+<C-S-F9><C-S-F8>#!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/edev_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
